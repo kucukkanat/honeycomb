c14eedbe800ce183828e0a44fc2c2145beff3320
kucukkanat <htolgasahin@gmail.com>
2021-05-01 02:17:25 +0200

    feat: finished rsa static methods

--- packages/wallet/crypto.js
+++ packages/wallet/crypto.js
@@ -1,6 +1,6 @@
-import { Logger} from "./log.js";
+import { Logger } from "./log.js";
 
-const format = {
+const RsaHashedKeyGenParams = {
   name: "RSA-OAEP",
   // Consider using a 4096-bit key for systems that require long-term security
   modulusLength: 2048,
@@ -8,21 +8,84 @@ const format = {
   hash: "SHA-256",
 };
 export class RSA {
-  static async GenerateKeyPair(modulusLength = 2048) {
-    const { privateKey, publicKey } = await crypto.subtle.generateKey({ ...format, modulusLength }, true, [
+  /**
+   * Automatically exports private and public keys upon generation
+   * @returns {Promise<{PrivateKey:string,PublicKey:string}>}
+   */
+  static async GenerateKeyPair() {
+    const { privateKey, publicKey } = await crypto.subtle.generateKey(RsaHashedKeyGenParams, true, [
       "encrypt",
       "decrypt",
     ]);
-    let exportedPublicKey = await crypto.subtle.exportKey("spki", publicKey)
-    // binary to ascii
-    exportedPublicKey = btoa(ArrayBufferTools.ab2str(exportedPublicKey));
+    let exportedPublicKey = await RSA.ExportPublicKeyAsString(publicKey)
 
-    let exportedPrivateKey = await crypto.subtle.exportKey("pkcs8", privateKey)
-    exportedPrivateKey = btoa(ArrayBufferTools.ab2str(exportedPrivateKey));
+    let exportedPrivateKey = await RSA.ExportPrivateKeyAsString(privateKey)
     Logger.log("Exporting keys")
     return { PrivateKey: exportedPrivateKey, PublicKey: exportedPublicKey }
   }
 
+  /**
+   * Imports a privatekey as a string and returns a CryptoKey
+   * @param {string} privatKeyString 
+   * @returns {CryptoKey}
+   */
+  static async ImportPrivateKey(privatKeyString) {
+    return await crypto.subtle.importKey(
+      "pkcs8",
+      ArrayBufferTools.str2ab(atob(privatKeyString)),
+      RsaHashedKeyGenParams,
+      true,
+      ["decrypt"]
+    );
+  }
+
+  /**
+   * Imports a public key as a string and returns a CryptoKey
+   * @param {string} privatKeyString 
+   * @returns {CryptoKey}
+   */
+  static async ImportPublicKey(publicKeyString) {
+    return await crypto.subtle.importKey(
+      "spki",
+      ArrayBufferTools.str2ab(atob(publicKeyString)),
+      RsaHashedKeyGenParams,
+      true,
+      ["encrypt"]
+    );
+  }
+
+  /**
+   * @param {CryptoKey} privateKey 
+   */
+  static async ExportPrivateKey(privateKey) {
+    return await crypto.subtle.exportKey("pkcs8", privateKey)
+  }
+  /**
+   * @param {CryptoKey} publicKey 
+   */
+  static async ExportPublicKey(publicKey) {
+    return await crypto.subtle.exportKey("spki", publicKey)
+  }
+
+  /**
+   * Takes a RSA private key and exports as string
+   * @param {CryptoKey} publicKey 
+   * @returns {string}
+   */
+  static async ExportPrivateKeyAsString(publicKey) {
+    let exported = await RSA.ExportPrivateKey(publicKey)
+    return btoa(ArrayBufferTools.ab2str(exported))
+  }
+
+  /**
+   * Takes a RSA public key and exports as string
+   * @param {CryptoKey} publicKey 
+   * @returns {string}
+   */
+  static async ExportPublicKeyAsString(publicKey) {
+    let exported = await RSA.ExportPublicKey(publicKey)
+    return btoa(ArrayBufferTools.ab2str(exported))
+  }
 
 }
 
@@ -47,16 +110,3 @@ class ArrayBufferTools {
     return buf;
   }
 }
-
-
-
-export async function ImportPrivateKey(base64PrivateKeyString) {
-  return await crypto.subtle.importKey(
-    "pkcs8",
-    str2ab(atob(base64PrivateKeyString)),
-    format,
-    true,
-    ["decrypt"]
-  );
-}
-
--- packages/wallet/crypto.test.js
+++ packages/wallet/crypto.test.js
@@ -1,7 +1,25 @@
 import { RSA } from "./crypto.js"
+import chai from "./tests/chai.esm.js"
+const { expect } = chai
+const { GenerateKeyPair, ImportPrivateKey, ExportPrivateKeyAsString, ExportPublicKeyAsString, ImportPublicKey } = RSA
 
 describe("generate key pair", async () => {
-    const { GenerateKeyPair } = RSA
-    const { PrivateKey, PublicKey } = await GenerateKeyPair(2048)
-    console.log({ PrivateKey, PublicKey })
+    let keypair;
+    before(async () => {
+        keypair = await GenerateKeyPair(2048)
+    })
+
+    it("expects to import and export the private key correctly", async () => {
+        const { PrivateKey } = keypair
+        const importedPrivateKey = await ImportPrivateKey(PrivateKey)
+        const exportedPrivateKey = await ExportPrivateKeyAsString(importedPrivateKey)
+        expect(exportedPrivateKey).to.equal(PrivateKey)
+    })
+
+    it("expects to import and export the public key correctly", async () => {
+        const { PublicKey } = keypair
+        const importedPublicKey = await ImportPublicKey(PublicKey)
+        const exportedPublicKey = await ExportPublicKeyAsString(importedPublicKey)
+        expect(exportedPublicKey).to.equal(PublicKey)
+    })
 })
